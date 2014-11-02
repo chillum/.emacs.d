97a72032c740838f08fc8648776b6ed5fea65e5b
Vasily Korytov <vasily.korytov@yahoo.com>
2014-11-02 13:45:58 +0200

    clean up the default face

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "outline" :family "Menlo")))))
+ '(default ((t (:height 150 :family "Menlo")))))
 (package-initialize)
 (color-theme-initialize)
 (color-theme-solarized-dark)
