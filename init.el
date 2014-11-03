f4fc79d4691ce10b9fcb9640d561475ffcf80ac3
Vasily Korytov <vasily.korytov@yahoo.com>
2014-11-03 14:19:18 +0200

    autocomplete starts from the first letter

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-start 1)
  '(ahg-global-key-prefix "h")
  '(auto-save-list-file-prefix nil)
  '(blink-cursor-mode nil)
